4e6e7b2cc5c9947c72831204e924f87ec82b6022
Chris St. John <csjcode@gmail.com>
2023-02-20 17:41:36 -0600

    Product info

--- lib/products.ts
+++ lib/products.ts
@@ -1,16 +1,16 @@
 export const products = [
   {
-    id: 'handbag-oaxaca-sm-001',
-    name: 'Oaxaca handbag',
-    description: 'A small-sized handcrafted Oaxaca handbag',
+    id: 'handbag-blue-sm-001',
+    name: 'Blue handbag',
+    description: 'A small-sized handcrafted blue handbag',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
-    id: 'handbag-yuactan-med-001',
-    name: 'Yucatan handbag',
-    description: 'A medium-sized handcrafted Oaxaca handbag',
+    id: 'handbag-yellow-med-001',
+    name: 'Yellow handbag',
+    description: 'A medium-sized handcrafted yellow handbag',
     unitName: 'basket',
     priceSol: 0.1,
     priceUsd: 10,
